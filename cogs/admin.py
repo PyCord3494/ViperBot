7e89e19f47a0a80a163dd8807915cbaab704551a
MrBizWiz3494 <wilsonis123@icloud.com>
2019-07-18 16:56:46 -0500

    Added ban, banlist, and unban

--- cogs/admin.py
+++ cogs/admin.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 
+import config
+
 
 class Admin(commands.Cog):
 	def __init__(self, bot):
@@ -11,6 +13,7 @@ class Admin(commands.Cog):
 	#@has_permissions(administrator=True)
 	async def end(self, ctx):
 		await self.bot.logout()
+
 
 	# kick mentioned user
 	@commands.command()
@@ -24,14 +27,49 @@ class Admin(commands.Cog):
 	@commands.command()
 	#@has_permissions(ban_members=True)
 	async def ban(self, ctx, ban_user: discord.Member, reason_for_ban):
-		await ctx.guild.ban(ban_user, reason = reason_for_ban)
-		await ctx.send(f"User {ctx.author.mention} has banned {ban_user.name} {ban_user.id} for reason: {reason}")
+		try:
+			await ban_user.send(f"{config.banMsg}")
+			await ctx.guild.ban(ban_user, reason = reason_for_ban)
+			await ctx.send(f"User {ctx.author.mention} has banned {ban_user.name} {ban_user.id} for reason: {reason}")
+		except discord.Forbidden:
+			await ctx.send("But I can't, muh permissions >:c")
+		except discord.HTTPException:
+			await ctx.send("Sorry, I failed to unban that user!") 
+
+
+	# get ban list
+	@commands.command()
+	#@has_permissions(ban_members=True)
+	async def banlist(self, ctx):
+		bans = await ctx.guild.bans()
+		if bans:
+			users = "**Username | Reason**\n"
+			for ban in bans:
+				users += f"{ban[1]} | {ban[0]}\n"
+			await ctx.send(users)
+		else:
+			await ctx.send("No bans found!")
+
+
+	@commands.command()
+	#@has_permissions(ban_members=True)
+	async def unban(self, ctx, member_id: int):
+		try:
+			member = self.bot.get_user(member_id)
+			await ctx.guild.unban(member)
+			await ctx.send(f"User {ctx.author.mention} has unbanned {member.name} {member.id}")
+		except discord.Forbidden:
+			await ctx.send("But I can't, muh permissions >:c")
+		except discord.HTTPException:
+			await ctx.send("Sorry, I failed to unban that user!") 
+		except Exception as e:
+			await ctx.send(e)
 
 
 	# mute specific user
 	@commands.command()
 	#@has_permissions(manage_messages=True)
-	async def mute(self, ctx, mute_user: discord.Member):
+	async def mute(self, ctx, member):
 		guild = ctx.guild
 		mute_role = discord.utils.get(guild.roles, name = "Muted")
 		await mute_user.add_roles(mute_role) # assign the role
@@ -48,6 +86,7 @@ class Admin(commands.Cog):
 			await ctx.send(f"{ctx.message.author.mention} has unmuted {unmute_user.mention}")
 		except Exception as e:
 			await ctx.send(e)
+
 
 def setup(bot):
 	bot.add_cog(Admin(bot))
